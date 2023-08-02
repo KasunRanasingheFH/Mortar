a20a460e70469f4972514f47a10945ff50d0a7b6
SankaKodda <44859680+SankaKodda@users.noreply.github.com>
2023-08-02 12:02:57 +0530

    SalesAIAnalytics Yearly sales by quarter years value

--- src/test/java/com/mortarai/testcases/AIAnalyticsPageTests/SalesAIAnalyticsTest.java
+++ src/test/java/com/mortarai/testcases/AIAnalyticsPageTests/SalesAIAnalyticsTest.java
@@ -74,6 +74,7 @@ public class SalesAIAnalyticsTest extends TestBase {
         String value = salesAIAnalytics.verifyValue2023InYearlySalesByQuarter();
         Assert.assertEquals(value,"true","Not selected 2020");
     }
+
     @AfterMethod
     public void tearDown() {
         driver.quit();
